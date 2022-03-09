6227bff97689f54faabddf7a83cb49790106d498
BİLAL <bllsfk.3984@gmail.com>
2022-03-09 15:20:31 +0300

    Create operatorshw3.java

---
+++ operatorshw3.java
@@ -0,0 +1,19 @@
+package odev;
+
+public class operatorshw3 {
+    public static void main(String[] args) {
+        double radius= 5.5;
+        double perimeter=  2  *  Math.PI  *  radius;
+        double area=  radius  *  Math.PI  *  radius;
+
+        System.out.println("Perimeter = "+ perimeter);
+        // Perimeter = 34.55751918948772
+        System.out.println("Area = " +area);
+        //Area = 95.03317777109123
+
+
+
+    }
+
+
+}
