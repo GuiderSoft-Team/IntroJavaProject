39648d951ce725d850be61c65ae65cacce70b92d
NurannNalci <nuran.nalann2411@gmail.com>
2022-03-09 20:15:55 +0100

    MyFirstCommit

---
+++ src/MyHomeWork/FirstweekTask.java
@@ -0,0 +1,12 @@
+package MyHomeWork;
+
+public class FirstweekTask {
+    public static void main(String[] args) {
+        byte yas1=28;
+        byte yas2=35;
+
+        byte toplamYas=(byte)(yas1+yas2);
+
+        System.out.println("Toplam yaş = "+toplamYas);
+    }
+}
