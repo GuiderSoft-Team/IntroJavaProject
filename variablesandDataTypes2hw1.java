10a36fc5c130fb9f519516d8b1a487b1e481511e
BİLAL <bllsfk.3984@gmail.com>
2022-03-09 15:22:57 +0300

    Create variablesandDataTypes2hw1.java

---
+++ variablesandDataTypes2hw1.java
@@ -0,0 +1,23 @@
+package odev;
+
+public class variablesandDataTypes2hw1 {
+    public static void main(String[] args) {
+
+        String firstName=("Bilal");
+        System.out.println("firstName="+(firstName));
+
+        String lastName=("Safak");
+        System.out.println("lastName="+(lastName));
+
+        System.out.println("firstNameAndLastName="+(firstName +lastName));
+
+
+
+
+
+
+
+
+
+    }
+}
