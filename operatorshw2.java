2958c5e6a4a70195afaabaffa520efc5355f38bf
BİLAL <bllsfk.3984@gmail.com>
2022-03-09 15:14:39 +0300

    Create operatorshw2.java

---
+++ operatorshw2.java
@@ -0,0 +1,16 @@
+package odev;
+
+public class operatorshw2 {
+    public static void main(String[] args) {
+
+        int num1=36;
+        int num2=37;
+        int num3=38;
+
+        int sum= num1+num2+num3;
+        int average=sum/3;
+        System.out.println("Average="+average);
+
+    }
+
+}
