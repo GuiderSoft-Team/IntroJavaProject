5f6e89a1552a1c43e07bb68c40bbb513a8a7b7e9
BİLAL <bllsfk.3984@gmail.com>
2022-03-09 15:17:54 +0300

    Create javaintro.java

---
+++ javaintro.java
@@ -0,0 +1,20 @@
+package odev;
+
+public class javaintro {
+    public static void main(String[] args) {
+        String triangle= "*";
+        System.out.println("       *");
+        triangle="* *";
+        System.out.println("      * *");
+        triangle="     *   *";
+        System.out.println("     *   *");
+        triangle="    *******";
+        System.out.println("    *******");
+
+
+
+
+
+
+    }
+}
