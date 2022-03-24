52fff529048819c1555e04cd873c6af01090772d
BİLAL <bllsfk.3984@gmail.com>
2022-03-24 20:43:35 +0300

    Proje başlatıldı.

---
+++ src/calisma/Kare.java
@@ -0,0 +1,12 @@
+package calisma;
+
+public class Kare {
+    //Karenin özelliklerini bilirsem
+    //sonra görsellik ekleyeceğim çocuklar için
+    //gizli private mı olsun yoksa
+    double kenar;
+    public double alanHesapla() {
+
+
+    }
+}
